fc7f04f754da59def04af48cbd9a90f27e5a72b8
belenar96 <belensua123@gmail.com>
2018-08-07 10:56:51 -0500

    actualizado a la forma anterior

--- app/Http/Controllers/DocentesController.php
+++ app/Http/Controllers/DocentesController.php
@@ -13,6 +13,8 @@ use App\Persona;
 use App\Sexo;
 use App\Facultade;
 use App\User;
+use App\Curso;
+
 use DB;
 
 class DocentesController extends Controller
@@ -39,18 +41,34 @@ class DocentesController extends Controller
        // return view('docentes.index',compact('cursosdocentes'));
         
 
-        $id_curso= Auth::user()->id;
-        $cantidades = \DB::table('estudiantes')  
-        //->selectcount(*) 
-        ->join('estudiantedocentecursos','estudiantes.id','=','estudiantedocentecursos.estudiante_id') 
-        ->join('docentecursos','estudiantedocentecursos.docentecurso_id','=','docentecursos.id')
-        ->join('cursos','docentecursos.curso_id','=','cursos.id')
-        ->where('cursos.id', $id_user)
-       // ->where('cursos.id',$id) 
-        ->selectRaw('count(*) as Cantidad')
-        ->get();
-        return view('docentes.index',compact('cantidades','cursosdocentes'));
-        return view('admintle.blade',compact('cursosdocentes'));
+       // $id_curso= Auth::user()->id;   
+       // $cursos= Curso::pluck('id');
+      //  $cantidades = \DB::table('users')  
+       //->selectcount(*) 
+      //->join('personas', 'personas.user_id', '=', 'users.id')
+       // ->join('estudiantes', 'estudiantes.persona_id', '=', 'personas.id')
+       // ->join('estudiantedocentecursos', 'estudiantedocentecursos.estudiante_id', '=', 'estudiantes.id')
+       // ->join('docentecursos','estudiantedocentecursos.docentecurso_id','=','docentecursos.id')
+      // ->join('cursos','docentecursos.curso_id','=','cursos.id')
+      //  ->where('users.id', $id_user)
+     //   ->where('cursos.id', '2')
+        //->where('cursos.id',$id) 
+       // ->selectRaw('count(*) as Cantidad')
+      //  ->get();
+     //   return view('docentes.index',compact('cantidades','cursosdocentes'));
+     //   return view('admintle.blade',compact('cursosdocentes'));
+     $id_curso= Auth::user()->id;
+     $cantidades = \DB::table('estudiantes')  
+     //->selectcount(*) 
+     ->join('estudiantedocentecursos','estudiantes.id','=','estudiantedocentecursos.estudiante_id') 
+     ->join('docentecursos','estudiantedocentecursos.docentecurso_id','=','docentecursos.id')
+     ->join('cursos','docentecursos.curso_id','=','cursos.id')
+     ->where('cursos.id', '1')
+    // ->where('cursos.id',$id) 
+     ->selectRaw('count(*) as Cantidad')
+     ->get();
+     return view('docentes.index',compact('cantidades','cursosdocentes'));
+     return view('admintle.blade',compact('cursosdocentes'));
 
 
 
@@ -98,47 +116,7 @@ class DocentesController extends Controller
      */
     public function show($id)
     {
-        $departamentos = Departamento::pluck('nombre', 'id');
-
-        $id_user = Auth::user()->id;
-
-       $personaUser=\DB::table('users')  
-        ->select('users.id as id','personas.nombre as nombre','personas.primerapellido as primerapellido',
-        'personas.segundoapellido as segundoapellido','personas.dni as dni','personas.contacto as celular',
-        'personas.direccion as direccion','docentes.codigo as codigo','docentes.profesion as profesion',
-        'docentes.grado as grado','docentes.tutor as tutor','docentes.supertutor as supertutor',
-        'facultades.sigla as facultad','departamentos.nombre as departamento','users.foto as foto',
-        'personas.nacimiento as nacimiento','sexos.nombre as Genero','users.email as email')
-        ->join('personas','personas.user_id','=','users.id')
-        ->join('docentes','docentes.persona_id','=','personas.id')
-        ->join('sexos','sexos.id','=','personas.sexo_id')
-        ->join('docentecursos','docentecursos.docente_id','=','docentes.id')
-        ->join('cursos','cursos.id','=','docentecursos.curso_id')
-        ->join('escuelas','escuelas.id','=','cursos.escuela_id')
-        ->join('facultades','facultades.id','=','escuelas.facultade_id')
-        ->join('departamentos','departamentos.id','=','docentes.departamento_id')        
-        ->where('users.id', $id_user)
-        ->first();
-        //dd($personaUser);
-        //die;
-        //  return view('docentes.show',(['personaUser'=>$personaUser]))  ;
-       //alternativa return view('docentes.show',compact('sexos',$personaUser))  ;
-       // return view('docentes.show',compact('docenteUser'));
-        //return view('docentes.show')->with('personaUser',$personaUser);
-        $id_user = Auth::user()->id;
-        $cursosdocentes = \DB::table('users')        
-        ->select('cursos.nombre as Nombre','escuelas.sigla AS Escuela', 'facultades.sigla as Facultad')
-        ->join('personas', 'personas.user_id', '=', 'users.id')
-        ->join('docentes','docentes.persona_id','=','personas.id')     
-        ->join('docentecursos','docentecursos.docente_id','=','docentes.id')
-        ->join('cursos','docentecursos.curso_id','=','cursos.id')
-        ->join('escuelas','cursos.escuela_id','=','escuelas.id')
-        ->join('facultades','escuelas.facultade_id','=','facultades.id')         
-        ->where('users.id', $id_user)
-        //->groupby('cursos.nombre', 'escuelas.sigla' , 'facultades.sigla')
-        ->get();
-
-        return view('docentes.show',compact('personaUser','departamentos','cursosdocentes'));
+        
     }
 
     /**
@@ -149,47 +127,8 @@ class DocentesController extends Controller
      */
     public function edit($id)
     {
-        $id_user = Auth::user()->id;
-        $departamentos = Departamento::pluck('nombre', 'id');
-        $sexos = Sexo::pluck('nombre', 'id');
-        $facultades = Facultade::pluck('sigla', 'id');
-        $users= User::find($id);
         
-        $personaUser=\DB::table('users')  
-         ->select('users.id as id','personas.nombre as nombre','personas.primerapellido as primerapellido',
-         'personas.segundoapellido as segundoapellido','personas.dni as dni','personas.contacto as celular',
-         'personas.direccion as direccion','docentes.codigo as codigo','docentes.profesion as profesion',
-         'docentes.grado as grado','facultades.sigla as facultad','docentes.departamento_id as departamentoid',
-         'personas.nacimiento as nacimiento','personas.sexo_id as Genero','users.email as email',
-         'users.password as contraseña')
-         ->join('personas','users.id','=','personas.user_id')
-         ->join('docentes','docentes.persona_id','=','personas.id')
-         ->join('sexos','sexos.id','=','personas.sexo_id')
-         ->join('docentecursos','docentecursos.docente_id','=','docentes.id')
-         ->join('cursos','cursos.id','=','docentecursos.curso_id')
-         ->join('escuelas','escuelas.id','=','cursos.escuela_id')
-         ->join('facultades','facultades.id','=','escuelas.facultade_id')
-         ->join('departamentos','departamentos.id','=','docentes.departamento_id')
-         
-         ->where('users.id', $id_user)
-         ->first();
-
-
-         $id_user = Auth::user()->id;
-        $cursosdocentes = \DB::table('users')        
-        ->select('cursos.nombre as Nombre','escuelas.sigla AS Escuela', 'facultades.sigla as Facultad')
-        ->join('personas', 'personas.user_id', '=', 'users.id')
-        ->join('docentes','docentes.persona_id','=','personas.id')     
-        ->join('docentecursos','docentecursos.docente_id','=','docentes.id')
-        ->join('cursos','docentecursos.curso_id','=','cursos.id')
-        ->join('escuelas','cursos.escuela_id','=','escuelas.id')
-        ->join('facultades','escuelas.facultade_id','=','facultades.id')         
-        ->where('users.id', $id_user)
-        //->groupby('cursos.nombre', 'escuelas.sigla' , 'facultades.sigla')
-        ->get();
-         //dd($personaUser);
-         //return view('docentes.edit')->with('personaUser',$departamentos,$personaUser);
-         return view('docentes.edit',compact('personaUser','sexos','facultades','departamentos','users','cursosdocentes'));
+       
     }
 
     /**
@@ -201,41 +140,8 @@ class DocentesController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $User = User::find($id);
-        $personaUser = Persona::find($User->persona_id);
-
-       // $personaUser -> departamento_id = $request -> departamentoid;
-        $foto = $request ->file('foto');
-
-        if($foto!=null)
-        {
-            $user = User::find($id);
-            $name_photo = $foto->getClientOriginalName()."".$id;
-            $foto->move(public_path()."/imagenes/users/",$name_photo);
-            $user->foto = $name_photo;
-            $user->save();
-        }
-       // $personaUser -> nombre = $request -> nombre;
-        //$personaUser -> primerapellido = $request -> primerapellido;
-        //$personaUser -> segundoapellido = $request -> segundoapellido;
-        //$personaUser -> dni = $request -> dni;
-        $personaUser -> contacto = $request -> celular;
-        $personaUser -> direccion = $request -> direccion;
-
-      //  $personaUser -> nacimiento = $request -> nacimiento;
-        $docenteUser = Docente::find($id);
-        
-        $User -> email = $request -> email;
-
-       $docenteUser -> departamento_id = $request -> departamentoid;
-       //$docenteUser -> grado = $request -> grado;
-       //$docenteUser -> profesion = $request -> profesion;
-
-       //$personaUser-> sexo_id = $request -> Genero;
-       $User -> save();       
-        $personaUser  -> save();
-        $docenteUser -> save();
-        return redirect()->action('DocentesController@show',['$User -> id']);
+        //$personaUser= Persona::find($id);
+       
     }
 
     /**
